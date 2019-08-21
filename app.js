7043c4d3c8c8b1d087ce415bdaa72d2abeeffa13
jacobmoses10 <jacob@alarmin.com.au>
2019-08-21 22:13:33 +1000

    Connect to Firebase
    Realtime Database test

---
+++ app.js
@@ -0,0 +1,26 @@
+    
+    //Initialise Firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyC4UZh7En4OQdXSB_4ttY7foqlY6m-n6pM",
+        authDomain: "ses1database.firebaseapp.com",
+        databaseURL: "https://ses1database.firebaseio.com",
+        projectId: "ses1database",
+        storageBucket: "ses1database.appspot.com",
+        messagingSenderId: "685925628486",
+        appId: "1:685925628486:web:a275e836fa2cc0c4"
+      };
+      firebase.initializeApp(firebaseConfig);
+
+      // TEST
+      // Get elements
+      const preObject = document.getElementById('object');
+
+      // Create references
+      const dbRefObject = firebase.database().ref().child('object');
+
+      // Sync object changes
+      dbRefObject.on('value', snap => {
+        preObject.innerText = JSON.stringify(snap.val(), null, 3);
+        
+      });
+    
