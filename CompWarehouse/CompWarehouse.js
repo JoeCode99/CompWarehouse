c9089655a32e46941935ace84eceac0496b6f05b
jacobmoses10 <jacob@alarmin.com.au>
2019-08-30 12:06:13 +1000

    Connect Database

--- CompWarehouse/CompWarehouse.js
+++ CompWarehouse/CompWarehouse.js
@@ -1,3 +1,15 @@
+//Initialise Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyC4UZh7En4OQdXSB_4ttY7foqlY6m-n6pM",
+  authDomain: "ses1database.firebaseapp.com",
+  databaseURL: "https://ses1database.firebaseio.com",
+  projectId: "ses1database",
+  storageBucket: "ses1database.appspot.com",
+  messagingSenderId: "685925628486",
+  appId: "1:685925628486:web:a275e836fa2cc0c4"
+};
+firebase.initializeApp(firebaseConfig);
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
