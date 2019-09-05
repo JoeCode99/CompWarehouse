85822ea75f54b048034128a222f3cb146cc579ec
jacobmoses10 <jacob@alarmin.com.au>
2019-09-06 00:30:27 +1000

    Sign Up, Login, Logout Working

--- CompWarehouse/HTML/JS/Authentication.js
+++ CompWarehouse/HTML/JS/Authentication.js
@@ -1,21 +1,47 @@
-// Get Elements
-const emailTxt = document.getElementById("email");
-const passwordTxt = document.getElementById("pass");
-const loginBtn = document.getElementById("loginBtn");
+(function() {
 
-const firstNameTxt = document.getElementById("firstName");
-const lastNameTxt = document.getElementById("lastName");
-const emailTxt2 = document.getElementById("emailAddress");
-const passwordTxt2 = document.getElementById("password");
-const addressTxt = document.getElementById("billingAddress");
-const registerBtn = document.getElementById("registerBtn");
+    // Get Elements
+    const emailTxt = document.getElementById("email");
+    const passwordTxt = document.getElementById("pass");
+    const loginBtn = document.getElementById("loginBtn");
+    const logoutBtn = document.getElementById("logoutBtn");
+    const registerBtn = document.getElementById("registerBtn");
 
-//Login Form
-loginBtn.addEventListener('click', e => {
-    const email = emailTxt.value;
-    const pass = passwordTxt.value;
-    const auth = firbase.auth();
+    // const firstNameTxt = document.getElementById("firstName");
+    // const lastNameTxt = document.getElementById("lastName");
+    // const emailTxt2 = document.getElementById("emailAddress");
+    // const passwordTxt2 = document.getElementById("password");
+    // const addressTxt = document.getElementById("billingAddress");
+    
 
-    const promise = auth.signInWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-})
+    loginBtn.addEventListener('click', e => {
+        const email = emailTxt.value;
+        const pass = passwordTxt.value;
+        const auth = firebase.auth();
+
+        const promise = auth.signInWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
+
+    registerBtn.addEventListener('click', e => {
+        const email = emailTxt.value;
+        const pass = passwordTxt.value;
+        const auth = firebase.auth();
+
+        const promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+    });
+
+    logoutBtn.addEventListener('click', e => {
+        firebase.auth().signOut();
+    });
+
+    // Add a realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log(firebaseUser);
+        } else {
+            console.log('not logged in');
+        }
+    });
+}());
