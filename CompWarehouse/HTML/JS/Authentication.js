44dcc4e1ce0d355893deb43c3fc076b52bcff46a
Joseph <dacalculator@gmail.com>
2019-09-06 13:28:39 +1000

    Error Messages for login

--- CompWarehouse/HTML/JS/Authentication.js
+++ CompWarehouse/HTML/JS/Authentication.js
@@ -12,28 +12,62 @@
     // const emailTxt2 = document.getElementById("emailAddress");
     // const passwordTxt2 = document.getElementById("password");
     // const addressTxt = document.getElementById("billingAddress");
-    
 
     loginBtn.addEventListener('click', e => {
         const email = emailTxt.value;
         const pass = passwordTxt.value;
         const auth = firebase.auth();
 
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
+        auth.signInWithEmailAndPassword(email, pass).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // [START_EXCLUDE]
+            if (errorCode.length >= 5) {
+              alert('Email address or password is incorrect');
+            } else {
+              alert(errorMessage);
+            }
+            console.log(error);
+            // [END_EXCLUDE]
+          });
+
+        /*const promise = auth.signInWithEmailAndPassword(email, pass);
+
+        var errorCode = error.code;
+        window.alert(error.message);
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/wrong-password') {
+            window.alert('Email or password is incorrect');
+          } else {
+            window.alert(errorMessage);
+          }
+          console.log(error);
+          promise.catch(e => console.log(e.message));
+          document.getElementById('quickstart-sign-in').disabled = false;*/
+
     });
 
     registerBtn.addEventListener('click', e => {
         const email = emailTxt.value;
         const pass = passwordTxt.value;
         const auth = firebase.auth();
-
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
+        if (pass.length < 6) {
+            window.alert("Your password must have 6 or more characters")
+        }
+        else if (!(/\S+@\S+\.\S+/.test(email))) {
+            window.alert("Invalid email address")
+        }
+        else {
+            const promise = auth.createUserWithEmailAndPassword(email, pass);
+            promise.catch(e => console.log(e.message));
+        }
     });
 
     logoutBtn.addEventListener('click', e => {
         firebase.auth().signOut();
+        window.location.replace("CompWarehouse.html")
     });
 
     // Add a realtime listener
@@ -41,7 +75,7 @@
         if (firebaseUser) {
             console.log(firebaseUser);
         } else {
-            console.log('not logged in');
+            console.log('Not logged in');
         }
     });
 }());
